7ae2872306e6e6ad93348a90bc683ded9cc4deea
MagusConjurer <cameronisadavis@gmail.com>
2020-04-11 22:13:41 -0600

    Added input form

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyAEkFoE-U1ewYotpASpQdB-QU4PCm5ysMA",
+  authDomain: "cdavis-4220.firebaseapp.com",
+  databaseURL: "https://cdavis-4220.firebaseio.com",
+  projectId: "cdavis-4220",
+  storageBucket: "cdavis-4220.appspot.com",
+  messagingSenderId: "396141472765",
+  appId: "1:396141472765:web:26edd119f69cb6344b0794",
+  measurementId: "G-XEQ2WJF30K"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+
